75c76768a57fe1cbf96b2b8d4888245be1a90aac
samirayahayaibrahim <samirayahayaibraheem@gmail.com>
2025-03-27 07:45:22 -0700

    updated mongodb url

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use((req, res, next)=>{
 app.use('/api/flowerRoutes', flowerRoutes)
 
 //connect to mangoDB
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
         
 app.listen(process.env.PORT, () =>{
