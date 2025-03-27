58aa4270b8d82213a34d03ecf5ab58472be4d9c6
samirayahayaibrahim <samirayahayaibraheem@gmail.com>
2025-03-27 09:45:30 -0700

    installed cors

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require("cors");
 const flowerRoutes = require("./routes/flowerRoutes")
 
 const app = express()
 
+const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:5000";
+
 //middleware
+app.use(cors());
+
 app.use(express.json())
 
 app.use((req, res, next)=>{
@@ -20,8 +25,8 @@ app.use('/api/flowerRoutes', flowerRoutes)
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         
-app.listen(process.env.PORT, () =>{
-    console.log('connected to DB & listening on port', process.env.PORT)
+app.listen(process.env.PORT || 4000, () =>{
+    console.log('connected to DB & listening on port', process.env.PORT || 4000)
 })
     })
     .catch((error) => {
