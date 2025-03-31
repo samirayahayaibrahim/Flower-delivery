1a381ddb7a0263eacf2bc30c7568d7464012b1f8
samirayahayaibrahim <samirayahayaibraheem@gmail.com>
2025-03-31 07:35:58 -0700

    create a user route and added a post for signup and login

---
+++ routes/userRouter.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const router = express.Router()
+
+//login
+router.post('/login', () => {})
+
+//signup
+router.post('/signup', () => {})
+
+
+
+module.exports = router
