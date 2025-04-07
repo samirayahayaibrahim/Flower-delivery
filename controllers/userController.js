bf814515795c22844dff213f1ef7a6f16d216825
samirayahayaibrahim <samirayahayaibraheem@gmail.com>
2025-04-07 03:15:22 -0700

    added jwt to my code

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const User = require('../models/userModel')
 const jwt = require('jsonwebtoken')
 
 const createToken = (_id) => {
-
+    jwt.sign({_id}, process.env.SECRET, { expiresIn: "2d" })
 }
 
 //login-user
