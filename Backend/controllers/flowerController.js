d64c12a120b2669a65a188de0adfc543244889f5
samirayahayaibrahim <samirayahayaibraheem@gmail.com>
2025-04-15 07:59:31 -0700

    created a react app in frontend folder

---
+++ Backend/controllers/flowerController.js
@@ -0,0 +1,81 @@
+const Flower = require("../models/flowerModel")
+const mongoose = require('mongoose')
+
+//all flower
+const getFlowers = async (req, res) => {
+    const flowers = await Flower.find({}).sort({createedAt: -1})
+
+    res.status(200).json(flowers)
+}
+
+
+//single flower
+const getFlower = async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: 'No such flower'})
+    }
+
+    const flower = await Flower.findById(id)
+
+    if (!flower) {
+    return res.status(404).json({error: 'no such flower'})
+}
+    res.status(200).json(flower)
+}
+
+//create new flower & connected to db
+const createFlower = async (req, res) => {
+    const {name, image, description, price, category} = req.body
+    
+    try{
+        const flower = await Flower.create({name, image, description, price, category})
+        res.status(200).json(flower)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+
+    }
+}
+
+const deleteFlower = async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: 'No such flower'})
+    }
+
+    const flower = await Flower.findOneAndDelete({_id: id})
+
+    if (!flower) {
+        return res.status(400).json({error: 'no such flower'})
+    }
+
+    res.status(200).json(flower)
+}
+
+const updateFlower = async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: 'No such flower'})
+    }
+
+    const flower = await Flower.findOneAndUpdate({_id: id}, {
+        ...req.body
+    })
+
+    if (!flower) {
+        return res.status(400).json({error: 'no such flower'})
+    }
+
+    res.status(200).json(flower)
+}
+
+module.exports = {
+    getFlower,
+    getFlowers,
+    createFlower,
+    deleteFlower,
+    updateFlower
+}
---
+++ Backend/server.js
@@ -0,0 +1,37 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require("cors");
+const flowerRoutes = require("./routes/flowerRoutes")
+const userRoutes = require("./routes/userRoutes")
+
+const app = express()
+
+const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:4000";
+
+//middleware
+app.use(cors());
+
+app.use(express.json())
+
+app.use((req, res, next)=>{
+    console.log(req.path, req.method)
+    next()
+})
+
+//routes
+app.use('/api/flowerRoutes', flowerRoutes)
+app.use('/api/user', userRoutes)
+
+//connect to mangoDB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        
+app.listen(process.env.PORT, () =>{
+    console.log('connected to DB & listening on port', process.env.PORT || 4000)
+})
+    })
+    .catch((error) => {
+        console.log(error)
+    })
