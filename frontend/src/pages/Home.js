969ad69d44b66c25b252a92a4f85b0ee6d23a8ea
samirayahayaibrahim <samirayahayaibraheem@gmail.com>
2025-04-23 07:47:03 -0700

    added fetch api to my home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
 
 const Home = () => {
+    const [flowers, setFlowers] = useState(null)
+
+    useEffect(() => {
+        const fetchFlowers = async () => {
+            const response = await fetch('/api/flowerRoutes/')
+            const json = await response.json()
+
+            if (response.ok) {
+                setFlowers(json)
+            }
+        }
+
+        fetchFlowers()
+    }, [])
     return ( 
         <div className="homepage">
-            <h1>hello</h1>
+            <div className="flowers">
+                {flowers && flowers.map((flower) =>(
+                <p key={flower._id}>{flower.name}</p>
+                ))}
+            </div>
         </div>
     );
 }
